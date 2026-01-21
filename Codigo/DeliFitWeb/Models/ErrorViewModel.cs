ba5819ca653f1719047eacca034c23cc979a6ef8
PauloHenrique1O <ph30459@gmail.com>
2026-01-21 20:53:06 -0300

    Apagando arquivos da implementação antiga Issue #67

--- Codigo/DeliFitWeb/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace DeliFitWeb.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
