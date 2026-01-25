732e4eebe7f3277aca1c8b710438d261add5f098
Leandro Carvalho <151436121+LeandroMCarv@users.noreply.github.com>
2026-01-25 16:47:05 -0300

    Map de ItemViewModel implementado

---
+++ Codigo/DeliFitWeb/Mappers/ItemProfile.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using Core;
+using DeliFitWeb.Models;
+
+namespace DeliFitWeb.Mappers
+{
+    public class ItemProfile : Profile
+    {
+        public ItemProfile()
+        {
+            CreateMap<ItemViewModel,Item>().ReverseMap();
+        }
+    }
+}
