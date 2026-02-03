f4af86db7d566d6f0228fd2d7868d4bff29a4442
Kauanfbr <kauanfbr10@gmail.com>
2026-02-03 20:42:50 -0300

    Criado os Service, Interface, Controller, Mapper, Model e View dos casos de uso Manter Cartao e Manter Carrinho Issue #101

---
+++ Codigo/DeliFitWeb/Mappers/CartaoProfile.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using Core;
+using Core.DTO;
+using DeliFitWeb.Models;
+
+namespace DeliFitWeb.Mappers;
+
+public class CartaoProfile : Profile
+{
+    public CartaoProfile()
+    {
+        CreateMap<Cartao, CartaoViewModel>().ReverseMap();
+    }
+}
