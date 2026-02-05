44e8d8231de0c3406a0fdadcea44c331ec9e0e19
Leandro Carvalho <151436121+LeandroMCarv@users.noreply.github.com>
2026-02-04 21:27:51 -0300

    adicionado as telas necessárias para funcionamento de solicitacao de um restaurante

--- Codigo/DeliFitWeb/Controllers/RestauranteController.cs
+++ Codigo/DeliFitWeb/Controllers/RestauranteController.cs
@@ -20,7 +20,24 @@ namespace DeliFitWeb.Controllers
         // GET: RestauranteController
         public ActionResult Index()
         {
-            var listaRestaurantes = _restauranteService.GetAll();
+            // FILTRO: Apenas restaurantes ATIVOS (Validado = true)
+            var listaRestaurantes = _restauranteService.GetAll()
+                .Where(r => r.Validado == true) // <-- ADICIONE ESTE FILTRO
+                .ToList();
+
+            var listaRestaurantesModel = _mapper.Map<List<RestauranteViewModel>>(listaRestaurantes);
+
+            return View(listaRestaurantesModel);
+        }
+
+        // GET: RestauranteController/ListarSolicitacoes
+        public ActionResult ListarSolicitacoes()
+        {
+            // FILTRO: Apenas restaurantes INATIVOS/PENDENTES (Validado = false)
+            var listaRestaurantes = _restauranteService.GetAll()
+                .Where(r => r.Validado == false) // <-- ADICIONE ESTE FILTRO
+                .ToList();
+
             var listaRestaurantesModel = _mapper.Map<List<RestauranteViewModel>>(listaRestaurantes);
 
             return View(listaRestaurantesModel);
@@ -31,6 +48,19 @@ namespace DeliFitWeb.Controllers
         {
             Restaurante? restaurante = _restauranteService.Get(id);
             RestauranteViewModel restauranteModel = _mapper.Map<RestauranteViewModel>(restaurante);
+
+            ViewBag.CanChangeStatus = false;
+
+            return View(restauranteModel);
+        }
+
+        // GET: RestauranteController/DetailsSolicitacao/5
+        public ActionResult DetailsSolicitacao(uint id)
+        {
+            Restaurante? restaurante = _restauranteService.Get(id);
+            RestauranteViewModel restauranteModel = _mapper.Map<RestauranteViewModel>(restaurante);
+
+            ViewBag.CanChangeStatus = true;
 
             return View(restauranteModel);
         }
@@ -84,7 +114,7 @@ namespace DeliFitWeb.Controllers
         {
             Restaurante? restaurante = _restauranteService.Get(id);
             RestauranteViewModel restauranteModel = _mapper.Map<RestauranteViewModel>(restaurante);
-            
+
             return View(restauranteModel);
         }
 
@@ -96,6 +126,51 @@ namespace DeliFitWeb.Controllers
             _restauranteService.Delete(id);
 
             return RedirectToAction(nameof(Index));
+        }
+
+        // GET: RestauranteController/SolicitarAdesao
+        public ActionResult SolicitarAdesao()
+        {
+            return View();
+        }
+
+        // POST: RestauranteController/SolicitarAdesao
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult SolicitarAdesao(RestauranteViewModel restauranteModel)
+        {
+
+            if (ModelState.IsValid)
+            {
+                var restaurante = _mapper.Map<Restaurante>(restauranteModel);
+                _restauranteService.Create(restaurante);
+            }
+
+            return RedirectToAction(nameof(Index));
+        }
+
+        // POST: RestauranteController/AprovarSolicitacao/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult AprovarSolicitacao(uint id)
+        {
+            var restaurante = _restauranteService.Get(id);
+            if (restaurante != null)
+            {
+                restaurante.Validado = true; // Ativa o restaurante
+                _restauranteService.Edit(restaurante);
+            }
+            return RedirectToAction(nameof(ListarSolicitacoes));
+        }
+
+        // POST: RestauranteController/NegarSolicitacao/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult NegarSolicitacao(uint id)
+        {
+            _restauranteService.Delete(id);
+
+            return RedirectToAction(nameof(ListarSolicitacoes));
         }
     }
 }
