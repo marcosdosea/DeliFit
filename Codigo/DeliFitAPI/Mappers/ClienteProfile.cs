c6d0c19d42a5e55ac1005b0ca96633a739937b99
PauloHenrique1O <ph30459@gmail.com>
2026-02-24 19:11:38 -0300

    Implementado API referente a cliente Issue #215

---
+++ Codigo/DeliFitAPI/Mappers/ClienteProfile.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using Core;
+using Core.DTO;
+using DeliFitAPI.Models;
+
+namespace DeliFitAPI.Mappers;
+
+public class ClienteProfile : Profile
+{
+    public ClienteProfile()
+    {
+        CreateMap<ClienteDTO, ClienteViewModel>().ReverseMap();
+        CreateMap<Cliente, ClienteViewModel>().ReverseMap();
+    }
+}
