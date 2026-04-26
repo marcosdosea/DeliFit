66155a13e63d80e4c103103853c7ce665eba5e43
Leandro Carvalho <151436121+LeandroMCarv@users.noreply.github.com>
2026-04-26 09:55:01 -0300

    feat: mapper da foto implementado

--- Codigo/DeliFitWeb/Mappers/RestauranteProfile.cs
+++ Codigo/DeliFitWeb/Mappers/RestauranteProfile.cs
@@ -2,16 +2,19 @@ using AutoMapper;
 using Core;
 using Core.DTO;
 using DeliFitWeb.Models;
+
 namespace DeliFitWeb.Mappers
 {
     public class RestauranteProfile : Profile
     {
         public RestauranteProfile()
         {
-            CreateMap<RestauranteViewModel, Restaurante>().ReverseMap();
+            CreateMap<Restaurante, RestauranteViewModel>()
+                .ForMember(dest => dest.FotoFile, opt => opt.Ignore())
+                .ReverseMap()
+                .ForMember(dest => dest.Foto, opt => opt.Ignore());
 
             CreateMap<RestauranteViewModel, RestauranteDTO>().ReverseMap();
         }
-
     }
 }
