e36bde7b486d7f867638c7035b2da9cb31b1f6c0
Leandro Carvalho <151436121+LeandroMCarv@users.noreply.github.com>
2026-05-08 11:40:02 -0300

    feat: arquivos para tornar possivel a avaliacao de um pedido

---
+++ Codigo/Core/Service/IAvaliacaoService.cs
@@ -0,0 +1,15 @@
+﻿using Core;
+
+namespace Core.Service
+{
+    public interface IAvaliacaoService
+    {
+        void Create(Avaliacao avaliacao);
+        Avaliacao? Get(uint id);
+        IEnumerable<Avaliacao> GetAll();
+        void Update(Avaliacao avaliacao);
+        void Delete(uint id);
+        IEnumerable<Avaliacao> GetByPedido(uint idPedido);
+        IEnumerable<Avaliacao> GetByCliente(uint idCliente);
+    }
+}
---
+++ Codigo/Service/AvaliacaoService.cs
@@ -0,0 +1,62 @@
+﻿using Core;
+using Core.Service;
+using Microsoft.EntityFrameworkCore;
+
+namespace Service
+{
+    public class AvaliacaoService : IAvaliacaoService
+    {
+        private readonly DeliFitContext _context;
+
+        public AvaliacaoService(DeliFitContext context)
+        {
+            _context = context;
+        }
+
+        public void Create(Avaliacao avaliacao)
+        {
+            _context.Avaliacaos.Add(avaliacao);
+            _context.SaveChanges();
+        }
+
+        public Avaliacao? Get(uint id)
+        {
+            return _context.Avaliacaos.Find(id);
+        }
+
+        public IEnumerable<Avaliacao> GetAll()
+        {
+            return _context.Avaliacaos.ToList();
+        }
+
+        public void Update(Avaliacao avaliacao)
+        {
+            _context.Entry(avaliacao).State = EntityState.Modified;
+            _context.SaveChanges();
+        }
+
+        public void Delete(uint id)
+        {
+            var avaliacao = _context.Avaliacaos.Find(id);
+            if (avaliacao != null)
+            {
+                _context.Avaliacaos.Remove(avaliacao);
+                _context.SaveChanges();
+            }
+        }
+
+        public IEnumerable<Avaliacao> GetByPedido(uint idPedido)
+        {
+            return _context.Avaliacaos
+                .Where(a => a.IdPedido == idPedido)
+                .ToList();
+        }
+
+        public IEnumerable<Avaliacao> GetByCliente(uint idCliente)
+        {
+            return _context.Avaliacaos
+                .Where(a => a.IdCliente == idCliente)
+                .ToList();
+        }
+    }
+}
